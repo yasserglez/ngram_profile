404ba2433012fa940a10c32ab8aa86419cb42063
Yasser Gonzalez Fernandez <ygonzalezfernandez@gmail.com>
2014-04-05 00:07:38 -0400

    Building a profile from a string, file or dir.

--- ngram_profile.py
+++ ngram_profile.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 
 """
-Text classification based on n-grams.
+Text classification based on character n-grams.
 """
 
+import os
+import codecs
 import json
+import itertools
+import operator
+import heapq
 
 
 class NGramProfile(object):
-    """Base class for n-gram profiles.
-
-    Subclass and override tokenize to provide an n-gram interpretation 
-    (i.e. character n-grams, word n-grams, etc).
-    """
+    """Character n-gram profile."""
 
     def __init__(self):
         """Initialize an empty profile."""
@@ -21,14 +22,66 @@ class NGramProfile(object):
     @classmethod
     def from_json(cls, file_path):
         """Load a profile previously saved in a JSON file."""
-        self = cls()
+        profile = cls()
         with open(file_path, 'r') as fd:
-            self._ngrams = json.load(fd)
-        return self
+            profile._ngrams = json.load(fd)
+        return profile
 
     @classmethod
-    def from_corpus(cls, corpus, ngram_size, profile_len, profile_offset):
-        """Build a profile from a collection of text files."""
+    def from_text(cls, text, ngram_sizes, profile_len, profile_offset):
+        """Build a profile from a UTF-8 encoded string."""
+        profile = cls()
+        profile._count_ngrams(text, ngram_sizes)
+        profile._normalize_ngram_freqs(ngram_sizes)
+        profile._build_ngram_profile(profile_len, profile_offset)
+        return profile
+
+    @classmethod
+    def from_file(cls, file_path, ngram_sizes, profile_len, profile_offset):
+        """Build a profile from a UTF-8 encoded text file."""
+        profile = cls()
+        with codecs.open(file_path, 'r', 'utf-8') as fd:
+            profile._count_ngrams(fd.read(), ngram_sizes)
+        profile._normalize_ngram_freqs(ngram_sizes)
+        profile._build_ngram_profile(profile_len, profile_offset)
+        return profile
+
+    @classmethod
+    def from_dir(cls, dir_path, ngram_sizes, profile_len, profile_offset):
+        """Build a profile from a directory containing UTF-8 encoded text files."""
+        profile = cls()
+        for dir_path, unused, file_names in os.walk(dir_path):
+            for file_name in file_names:
+                file_path = os.path.join(dir_path, file_name)
+                with codecs.open(file_path, 'r', 'utf-8') as fd:
+                    profile._count_ngrams(fd.read(), ngram_sizes)
+        profile._normalize_ngram_freqs(ngram_sizes)
+        profile._build_ngram_profile(profile_len, profile_offset)
+        return profile
+
+    def _count_ngrams(self, text, ngram_sizes):
+        text = self.normalize(text)
+        for ngram_size in ngram_sizes:
+            slices = [itertools.islice(text, i, None) for i in xrange(ngram_size)]
+            for ngram_tokens in itertools.izip(*slices):
+                ngram = u''.join(ngram_tokens)
+                self._ngrams[ngram] = self._ngrams.get(ngram, 0) + 1 
+
+    def _normalize_ngram_freqs(self, ngram_sizes):
+        for ngram_size in ngram_sizes:
+            ngram_count = 0.0
+            for ngram in self._ngrams.iterkeys():
+                if len(ngram) == ngram_size:
+                    ngram_count += self._ngrams[ngram]
+            for ngram in self._ngrams.iterkeys():
+                if len(ngram) == ngram_size:
+                    self._ngrams[ngram] = self._ngrams[ngram] / ngram_count
+
+    def _build_ngram_profile(self, profile_len, profile_offset):
+        top_ngrams = heapq.nlargest(profile_offset + profile_len, 
+                                    self._ngrams.iteritems(),
+                                    key=operator.itemgetter(1))
+        self._ngrams = dict(top_ngrams[profile_offset:])
 
     def __len__(self):
         """Number of n-grams in the profile."""
@@ -55,14 +108,6 @@ class NGramProfile(object):
         """Text normalization (identity function by default)."""
         return text
 
-    def tokenize(self, text):
-        """Split text into tokens.
-
-        Not implemented by default. Override in a subclass to provide an n-gram
-        interpretation (i.e. character n-grams, word n-grams, etc).
-        """
-        raise NotImplementedError()
-
     def jaccard_dissimilarity(self, other):
         """One minus the Jaccard similarity coefficient.
 
@@ -85,11 +130,3 @@ class NGramProfile(object):
         Categorization." In Proceedings of the 3rd Annual Symposium on Document
         Analysis and Information Retrieval, SDAIR'94, Las Vegas, US, pp. 161-175.
         """
-
-
-class CharNGramProfile(NGramProfile):
-    """Character-based n-gram profile."""
-
-    def tokenize(self, text):
-        """Split text into characters."""
-        return iter(text)
--- test_ngram_profile.py
+++ test_ngram_profile.py
@@ -3,62 +3,72 @@
 import os
 import json
 import unittest
+import codecs
 
-import ngram_profile
+from ngram_profile import NGramProfile
 
 
-class CommonNGramProfileTests(object):
-
-    profileClass = None
+class TestNGramProfile(unittest.TestCase):
 
     def test_init(self):
-        profile = self.profileClass()
+        profile = NGramProfile()
         self.assertEqual(len(profile), 0)
 
     def test_json_roundtrip(self):
         json_profile = '{"a": 0.5, "b": 0.3, "c": 0.2}'
         tmp_file = 'test_json_roundtrip.json'
-        with open(tmp_file, 'w') as fd:
+        with codecs.open(tmp_file, 'w', 'utf-8') as fd:
             fd.write(json_profile)
-        profile = self.profileClass.from_json(tmp_file)
+        profile = NGramProfile.from_json(tmp_file)
         os.remove(tmp_file)
         self.assertEqual(len(profile), 3)
         self.assertEqual(profile[u'a'], 0.5)
         self.assertEqual(profile[u'b'], 0.3)
         self.assertEqual(profile[u'c'], 0.2)
         profile.save_as_json(tmp_file)
-        with open(tmp_file, 'r') as fd:
+        with codecs.open(tmp_file, 'r', 'utf-8') as fd:
             self.assertEqual(json.load(fd), json.loads(json_profile))
         os.remove(tmp_file) 
 
-    def test_normalize_unicode_output(self):
-        profile = self.profileClass()
-        text = profile.normalize(u'abc')
-        self.assertTrue(isinstance(text, unicode))
-
-
-class TestNGramProfile(CommonNGramProfileTests, unittest.TestCase):
-
-    profileClass = ngram_profile.NGramProfile
-
     def test_normalize(self):
-        profile = self.profileClass()
         text = u'abc'
-        self.assertEqual(text, profile.normalize(text))
+        profile = NGramProfile()
+        normalized_text = profile.normalize(text)
+        self.assertTrue(isinstance(normalized_text, unicode))
+        self.assertEqual(normalized_text, text)
 
-    def test_tokenize(self):
-        profile = ngram_profile.NGramProfile()
-        self.assertRaises(NotImplementedError, profile.tokenize, u'')
+    def test_from_text_1gram(self):
+        text = u'abcaab'
+        ngram_sizes = (1, )
+        profile_len = 3
+        profile = NGramProfile.from_text(text, ngram_sizes, profile_len, 0)
+        self.assertEqual(len(profile), profile_len)
+        self.assertEqual(profile[u'a'], 0.5)
+        self.assertAlmostEqual(profile[u'b'], 0.33, delta=0.01)
+        self.assertAlmostEqual(profile[u'c'], 0.16, delta=0.01)
 
+    def test_from_text_2gram(self):
+        text = u'abcaab'
+        ngram_sizes = (2, )
+        profile_len = 4
+        profile = NGramProfile.from_text(text, ngram_sizes, profile_len, 0)
+        self.assertEqual(len(profile), profile_len)
+        self.assertEqual(profile[u'ab'], 0.4)
+        self.assertEqual(profile[u'bc'], 0.2)
+        self.assertEqual(profile[u'ca'], 0.2)
+        self.assertEqual(profile[u'aa'], 0.2)
 
-class TestCharNGramProfile(CommonNGramProfileTests, unittest.TestCase):
-
-    profileClass = ngram_profile.CharNGramProfile
-
-    def test_tokenize(self):
-        profile = ngram_profile.CharNGramProfile()
-        text = u'the quick brown fox jumps over the lazy dog'
-        self.assertEqual(list(text), list(profile.tokenize(text)))
+    def test_from_text_1gram_2gram_with_offset(self):
+        text = u'abcaab'
+        ngram_sizes = (1, 2)
+        profile_len = 4
+        profile_offset = 2
+        profile = NGramProfile.from_text(text, ngram_sizes, profile_len, profile_offset)
+        self.assertEqual(len(profile), profile_len)
+        self.assertAlmostEqual(profile[u'b'], 0.33, delta=0.01)
+        self.assertEqual(profile[u'bc'], 0.2)
+        self.assertEqual(profile[u'ca'], 0.2)
+        self.assertEqual(profile[u'aa'], 0.2)
 
 
 if __name__ == '__main__':
