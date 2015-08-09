91961becf381ff4ba16890759ae9358993b20fcc
Yasser Gonzalez <yasserglez@gmail.com>
2015-08-09 02:12:43 -0400

    Update the copyright notice

--- ngram_profile.py
+++ ngram_profile.py
@@ -1,11 +1,12 @@
 #
-# Copyright 2014 Yasser Gonzalez Fernandez
+# Copyright 2014-2015 Yasser Gonzalez Fernandez
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
+#
 
 """
 Text classification based on character n-grams.
